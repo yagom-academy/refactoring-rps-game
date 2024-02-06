d301de8e1bfe4979c8ca5fb9a133779eb8b35086
kangddong <rkdehddud96@naver.com>
2024-02-06 16:33:11 +0900

    [ADD] Test target

---
+++ RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -0,0 +1,19 @@
+//
+//  RockPaperScissorsTests.swift
+//  RockPaperScissorsTests
+//
+//  Created by 강동영 on 2/6/24.
+//
+
+import XCTest
+
+final class RockPaperScissorsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
