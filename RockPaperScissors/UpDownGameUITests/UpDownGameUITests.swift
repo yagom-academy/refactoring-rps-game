69068c815ddd98feb5535d43d1fedcd741df1ec2
sm-amoled <moktak072@naver.com>
2024-03-19 18:41:27 +0900

    refactor: 가위바위보 대전에 대한 테스트 작성

--- RockPaperScissors/UpDownGameUITests/UpDownGameUITests.swift
+++ RockPaperScissors/UpDownGameUITests/UpDownGameUITests.swift
@@ -33,6 +33,102 @@ final class UpDownGameUITests: XCTestCase {
         // then
         XCTAssertEqual(result, MatchResult.draw)
     }
+    
+    func test_양쪽에서_패를냈을때_묵_찌을_낸경우_이기기(){
+        // given
+        let myAction: Actions = .rock
+        let yourAction: Actions = .sissor
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.win)
+    }
+    
+    func test_양쪽에서_패를냈을때_묵_빠을_낸경우_지기(){
+        // given
+        let myAction: Actions = .rock
+        let yourAction: Actions = .paper
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.lose)
+    }
+    
+    func test_양쪽에서_패를냈을때_찌_묵을_낸경우_지기(){
+        // given
+        let myAction: Actions = .sissor
+        let yourAction: Actions = .rock
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.lose)
+    }
+    
+    func test_양쪽에서_패를냈을때_찌_찌을_낸경우_비기기(){
+        // given
+        let myAction: Actions = .sissor
+        let yourAction: Actions = .sissor
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.draw)
+    }
+    
+    func test_양쪽에서_패를냈을때_찌_빠을_낸경우_이기(){
+        // given
+        let myAction: Actions = .rock
+        let yourAction: Actions = .paper
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.win)
+    }
+    
+    func test_양쪽에서_패를냈을때_빠_묵을_낸경우_이기기(){
+        // given
+        let myAction: Actions = .paper
+        let yourAction: Actions = .rock
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.win)
+    }
+    
+    func test_양쪽에서_패를냈을때_빠_찌을_낸경우_지기기(){
+        // given
+        let myAction: Actions = .paper
+        let yourAction: Actions = .sissor
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.lose)
+    }
+    
+    func test_양쪽에서_패를냈을때_빠_빠을_낸경우_비기기(){
+        // given
+        let myAction: Actions = .paper
+        let yourAction: Actions = .paper
+        
+        // when
+        let result = sut.matchRPS(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.draw)
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
