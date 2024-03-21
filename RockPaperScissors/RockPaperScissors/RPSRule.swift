b00a5db3199cf8e09d64901574800452577ea3b9
MinyoungKoo <rnalsdud1014@naver.com>
2024-03-22 02:29:00 +0900

    [Test] 가위바위보 게임 규칙 테스트코드 추가 및 테스트를 위한 소스 수정
    최종 승리한 플레이어 판별, 게임 종료 판별, 점수 초기화 기능 테스트코드를 추가한다.
    테스트코드에서 값 비교를 위해 소스코드 class에서 Equtable을 채택한다.
    테스트코드에서 점수가 0인 상태의 인스턴스를 만들기 위해 convenience init을 추가한다.
    
    Fixes: https://camp.yagom-academy.kr/camp/65cc2b983173745319e0464c/projects/65cc320e3173745319e04663

--- RockPaperScissors/RockPaperScissors/RPSRule.swift
+++ RockPaperScissors/RockPaperScissors/RPSRule.swift
@@ -7,15 +7,29 @@
 
 import Foundation
 
-class Score {
-    var win: Int = 0
-    var draw: Int = 0
-    var lose: Int = 0
+class Score: Equatable {
+    static func == (lhs: Score, rhs: Score) -> Bool {
+        lhs.win == rhs.win && lhs.draw == rhs.draw && lhs.lose == rhs.lose
+    }
+    
+    var win: Int
+    var draw: Int
+    var lose: Int
     
     func reset() {
         self.win = 0
         self.draw = 0
         self.lose = 0
+    }
+    
+    convenience init() {
+        self.init(win: 0, draw: 0, lose: 0)
+    }
+    
+    init(win: Int, draw: Int, lose: Int) {
+        self.win = win
+        self.draw = draw
+        self.lose = lose
     }
 }
 
--- RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.swift
+++ RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -9,8 +9,19 @@ import XCTest
 @testable import RockPaperScissors
 
 final class RockPaperScissorsTests: XCTestCase {
-    func test_playerIsWinner() {
-        var rps = RPSGame()
+    private var rps: RPSGame!
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.rps = RPSGame()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_userIsWinner() {
         let messageWhenTestFails = "사용자 승리 판별 실패"
         
         rps.user.hand = .paper
@@ -26,8 +37,7 @@ final class RockPaperScissorsTests: XCTestCase {
         XCTAssertEqual(rps.userVersusComputer(), .userWon, messageWhenTestFails)
     }
     
-    func test_playerIsLoser() {
-        var rps = RPSGame()
+    func test_userIsLoser() {
         let messageWhenTestFails = "사용자 패배 판별 실패"
         
         rps.user.hand = .rock
@@ -43,8 +53,7 @@ final class RockPaperScissorsTests: XCTestCase {
         XCTAssertEqual(rps.userVersusComputer(), .userLost, messageWhenTestFails)
     }
     
-    func test_playerAndComputerAreDraw() {
-        var rps = RPSGame()
+    func test_userAndComputerAreDraw() {
         let messageWhenTestFails = "무승부 판별 실패"
         
         rps.user.hand = .rock
@@ -60,8 +69,37 @@ final class RockPaperScissorsTests: XCTestCase {
         XCTAssertEqual(rps.userVersusComputer(), .userDrew, messageWhenTestFails)
     }
     
-    // todo
-    // 3번 이긴 플레이어가 나왔을 때 끝나는지
-    // 초기화가 되는지
-    // 3번 이겼을 때 초기화 하는지
+    func test_whoIsFinalWinner() {
+        let messageWhenTestFails = "최종 승리한 플레이어 판별 실패"
+        rps.user.score.win = 3
+        rps.computer.score.win = 2
+        XCTAssertTrue(rps.finalWinner() is User, messageWhenTestFails)
+        
+        rps.user.score.win = 1
+        rps.computer.score.win = 3
+        XCTAssertTrue(rps.finalWinner() is Computer, messageWhenTestFails)
+    }
+    
+    func test_finish() {
+        let messageWhenTestFails = "게임 종료 판별 실패"
+        rps.user.score.win = 2
+        rps.computer.score.win = 2
+        XCTAssertEqual(rps.isFinished(), false, messageWhenTestFails)
+        
+        rps.user.score.win = 3
+        rps.computer.score.win = 0
+        XCTAssertEqual(rps.isFinished(), true, messageWhenTestFails)
+    }
+    
+    func test_reset() {
+        let messageWhenTestFails = "리셋 실패"
+        rps.user.score = Score(win: 3, draw: 2, lose: 1)
+        rps.computer.score = Score(win: 1, draw: 2, lose: 3)
+        rps.resetScore()
+       
+        let twoPlayersHaveTheSameScore = rps.user.score == rps.computer.score
+        let isZeroScore = rps.user.score == Score()
+        
+        XCTAssertTrue(twoPlayersHaveTheSameScore && isZeroScore, messageWhenTestFails)
+    }
 }
