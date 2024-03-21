2cf5e8dda12489412521198960d0976679e322b1
sm-amoled <moktak072@naver.com>
2024-03-21 17:34:58 +0900

    refactor: 묵찌빠 테스트코드 작성

--- RockPaperScissors/MJBTests/MJBTests.swift
+++ RockPaperScissors/MJBTests/MJBTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 final class MJBTests: XCTestCase {
 
-    var sut: MJBGameRule?
+    var sut: MJBGameRule!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -23,8 +23,228 @@ final class MJBTests: XCTestCase {
         sut = nil
     }
 
-    func testExample() throws {
+    func test_양쪽에서_패를냈을때_묵_묵을_낸경우_비기기(){
+        // given
+        let myAction: Actions = .rock
+        let yourAction: Actions = .rock
         
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.draw)
     }
-
+    
+    func test_양쪽에서_패를냈을때_묵_찌을_낸경우_이기기(){
+        // given
+        let myAction: Actions = .rock
+        let yourAction: Actions = .sissor
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.win)
+    }
+    
+    func test_양쪽에서_패를냈을때_묵_빠을_낸경우_지기(){
+        // given
+        let myAction: Actions = .rock
+        let yourAction: Actions = .paper
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.lose)
+    }
+    
+    func test_양쪽에서_패를냈을때_찌_묵을_낸경우_지기(){
+        // given
+        let myAction: Actions = .sissor
+        let yourAction: Actions = .rock
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.lose)
+    }
+    
+    func test_양쪽에서_패를냈을때_찌_찌을_낸경우_비기기(){
+        // given
+        let myAction: Actions = .sissor
+        let yourAction: Actions = .sissor
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.draw)
+    }
+    
+    func test_양쪽에서_패를냈을때_찌_빠을_낸경우_이기(){
+        // given
+        let myAction: Actions = .sissor
+        let yourAction: Actions = .paper
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.win)
+    }
+    
+    func test_양쪽에서_패를냈을때_빠_묵을_낸경우_이기기(){
+        // given
+        let myAction: Actions = .paper
+        let yourAction: Actions = .rock
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.win)
+    }
+    
+    func test_양쪽에서_패를냈을때_빠_찌을_낸경우_지기(){
+        // given
+        let myAction: Actions = .paper
+        let yourAction: Actions = .sissor
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.lose)
+    }
+    
+    func test_양쪽에서_패를냈을때_빠_빠을_낸경우_비기기(){
+        // given
+        let myAction: Actions = .paper
+        let yourAction: Actions = .paper
+        
+        // when
+        let result = sut.matchRule(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(result, MatchResult.draw)
+    }
+    
+    func test_내차례에_match에서비기면_승리하기() {
+        // given
+        sut.isMyTurn = true
+        let myAction: Actions = .paper
+        let yourAction: Actions = .paper
+        
+        
+        // when
+        sut.playGame(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(sut.gameStatus.gameResult, .win)
+    }
+    
+    func test_내차례에_match에서이기면_score변화없이내차례유지하기() {
+        // given
+        sut.isMyTurn = true
+        let myAction: Actions = .paper
+        let yourAction: Actions = .rock
+        
+        let originScore = sut.gameStatus.score
+        
+        // when
+        sut.playGame(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(sut.isMyTurn, true)
+        XCTAssertEqual(sut.gameStatus.score.win, originScore.win)
+        XCTAssertEqual(sut.gameStatus.gameResult, .playing)
+    }
+    
+    func test_상대차례에_match에서이기면_score변화없이내차례유지하기() {
+        // given
+        sut.isMyTurn = false
+        let myAction: Actions = .paper
+        let yourAction: Actions = .rock
+        
+        let originScore = sut.gameStatus.score
+        
+        // when
+        sut.playGame(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(sut.isMyTurn, true)
+        XCTAssertEqual(sut.gameStatus.score.win, originScore.win)
+        XCTAssertEqual(sut.gameStatus.gameResult, .playing)
+    }
+    
+    func test_내차례에_match에서지면_score변화없이상대차례유지하기() {
+        // given
+        sut.isMyTurn = true
+        let myAction: Actions = .rock
+        let yourAction: Actions = .paper
+        
+        let originScore = sut.gameStatus.score
+        
+        // when
+        sut.playGame(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(sut.isMyTurn, false)
+        XCTAssertEqual(sut.gameStatus.score.lose, originScore.lose)
+        XCTAssertEqual(sut.gameStatus.gameResult, .playing)
+    }
+    
+    func test_상대차례에_match에서지면_score변화없이상대차례유지하기() {
+        // given
+        sut.isMyTurn = false
+        let myAction: Actions = .rock
+        let yourAction: Actions = .paper
+        
+        let originScore = sut.gameStatus.score
+        
+        // when
+        sut.playGame(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(sut.isMyTurn, false)
+        XCTAssertEqual(sut.gameStatus.score.lose, originScore.lose)
+        XCTAssertEqual(sut.gameStatus.gameResult, .playing)
+    }
+    
+    func test_내차례에_match에서비기면_게임승리하기() {
+        // given
+        sut.isMyTurn = true
+        let myAction: Actions = .rock
+        let yourAction: Actions = .rock
+        
+        // when
+        sut.playGame(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(sut.gameStatus.gameResult, .win)
+    }
+    
+    func test_상대차례에_match에서비기면_게임패배하기() {
+        // given
+        sut.isMyTurn = false
+        let myAction: Actions = .rock
+        let yourAction: Actions = .rock
+        
+        // when
+        sut.playGame(myAction: myAction, opponentAction: yourAction)
+        
+        // then
+        XCTAssertEqual(sut.gameStatus.gameResult, .lose)
+    }
+    
+    
+    func test_내차례에_같은손을내면_승리하기() {
+        // given
+        
+        // when
+        
+        // then
+    }
 }
--- RockPaperScissors/RockPaperScissors/GameRules/MJBGameRule.swift
+++ RockPaperScissors/RockPaperScissors/GameRules/MJBGameRule.swift
@@ -11,18 +11,42 @@ class MJBGameRule : GameRule {
     var gameStatus: GameStatus
     var isMyTurn: Bool?
     
-    
     init(targetScore: Int) {
         self.gameStatus = GameStatus(targetScore: targetScore)
         self.isMyTurn = true
     }
     
     func matchRule(myAction: Actions, opponentAction: Actions) -> MatchResult {
+        switch (myAction, opponentAction) {
+        case (.rock, .paper), (.sissor, .rock), (.paper, .sissor):
+            return .lose
         
-        return .draw
+        case (.rock, .sissor), (.sissor, .paper), (.paper, .rock):
+            return .win
+            
+        default:
+            return .draw
+      }
     }
     
     func playGame(myAction: Actions, opponentAction: Actions) {
+        guard let isMyTurn else { return }
         
+        let matchResult = matchRule(myAction: myAction, opponentAction: opponentAction)
+        
+        switch matchResult {
+        case .draw:
+            if isMyTurn {
+                gameStatus.applyMatchResult(matchResult: .win)
+            } else {
+                gameStatus.applyMatchResult(matchResult: .lose)
+            }
+            
+        case .win:
+            self.isMyTurn = true
+            
+        case .lose:
+            self.isMyTurn = false
+        }
     }
 }
