5a9af0b7211d26fd9d2b888dbc0b8d10d3bc58f5
9min0-neowiz <9min0@neowiz.com>
2024-03-21 17:56:56 +0900

    [Test] 사용자의 승부 결과를 판정하는 테스트코드 추가
    사용자가 이긴 상황, 진 상황, 비긴 상황을 테스트하는 코드를 추가한다.
    
    Fixes: https://camp.yagom-academy.kr/camp/65cc2b983173745319e0464c/projects/65cc320e3173745319e04663

--- RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.swift
+++ RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -9,28 +9,59 @@ import XCTest
 @testable import RockPaperScissors
 
 final class RockPaperScissorsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_playerIsWinner() {
+        var rps = RPSGame()
+        let messageWhenTestFails = "사용자 승리 판별 실패"
+        
+        rps.user.hand = .paper
+        rps.computer.hand = .rock
+        XCTAssertEqual(rps.userVersusComputer(), .userWon, messageWhenTestFails)
+        
+        rps.user.hand = .rock
+        rps.computer.hand = .scissor
+        XCTAssertEqual(rps.userVersusComputer(), .userWon, messageWhenTestFails)
+        
+        rps.user.hand = .scissor
+        rps.computer.hand = .paper
+        XCTAssertEqual(rps.userVersusComputer(), .userWon, messageWhenTestFails)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_playerIsLoser() {
+        var rps = RPSGame()
+        let messageWhenTestFails = "사용자 패배 판별 실패"
+        
+        rps.user.hand = .rock
+        rps.computer.hand = .paper
+        XCTAssertEqual(rps.userVersusComputer(), .userLost, messageWhenTestFails)
+        
+        rps.user.hand = .scissor
+        rps.computer.hand = .rock
+        XCTAssertEqual(rps.userVersusComputer(), .userLost, messageWhenTestFails)
+        
+        rps.user.hand = .paper
+        rps.computer.hand = .scissor
+        XCTAssertEqual(rps.userVersusComputer(), .userLost, messageWhenTestFails)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_playerAndComputerAreDraw() {
+        var rps = RPSGame()
+        let messageWhenTestFails = "무승부 판별 실패"
+        
+        rps.user.hand = .rock
+        rps.computer.hand = .rock
+        XCTAssertEqual(rps.userVersusComputer(), .userDrew, messageWhenTestFails)
+        
+        rps.user.hand = .scissor
+        rps.computer.hand = .scissor
+        XCTAssertEqual(rps.userVersusComputer(), .userDrew, messageWhenTestFails)
+        
+        rps.user.hand = .paper
+        rps.computer.hand = .paper
+        XCTAssertEqual(rps.userVersusComputer(), .userDrew, messageWhenTestFails)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+    // todo
+    // 3번 이긴 플레이어가 나왔을 때 끝나는지
+    // 초기화가 되는지
+    // 3번 이겼을 때 초기화 하는지
 }
